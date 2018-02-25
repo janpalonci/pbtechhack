d7f3e55f7c6572b4bf6c8ba31cd7ec274f1eac16
Ihor Bodnarchuk <shef4u@inbox.ru>
2018-02-25 00:23:31 -0500

    added checkers

--- public/js/postCard.js
+++ public/js/postCard.js
@@ -1,12 +1,13 @@
-console.log('index online');
-let cardOut = false;
+console.log('post card online');
+
 const postURL = 'http://localhost:3000/posts';
 let loggedInLinkedIn = window.Cookies.get('linkedin');
 let loggedInFB = window.Cookies.get('facebook');
+let hashtags = [];
 
 $("#post-card-toggle").on('click', () => {
-	console.log('CLICK')
 $("#post-card-toggle").hide();
+
 let loggedIn = loggedInLinkedIn || loggedInFB;
 	if(loggedIn) {
 		$('#post-card-loggedIn').show();
@@ -16,6 +17,70 @@ let loggedIn = loggedInLinkedIn || loggedInFB;
 		$('#post-card-loggedIn').hide();
 		$('#post-card-loggedOut').show();
 	}
+
+	$('#card-pro').on('change', () => {
+		$("#card-pro-selected").slideToggle( "slow", function() {
+		// Animation complete.
+	});
+	});
+
+	$('#card-classroom').on('change', () => {
+
+			hashtags = [];
+			$("#card-hashtags").val("");
+			$("#card-internship").prop('checked', false)
+			$("#card-externship").prop('checked', false)
+			$("#card-mentorship").prop('checked', false)
+
+		$("#card-pro-selected").slideToggle( "slow", function() {
+		// Animation complete.
+	});
+	});
+
+	$("#card-internship").on('change', () => {
+		let val = "#internship";
+		if($("#card-internship").is(':checked')) {
+			if(!hashtags.includes(val)) {
+				hashtags.push(val);
+				$("#card-hashtags").val(hashtags);
+			}
+		}
+		else {
+			let index = hashtags.indexOf(val);
+			hashtags.splice(index, 1);
+			$("#card-hashtags").val(hashtags);
+		}
+	});
+
+	$("#card-mentorship").on('change', () => {
+		let val = "#mentorship";
+		if($("#card-mentorship").is(':checked')) {
+			if(!hashtags.includes(val)) {
+				hashtags.push(val);
+				$("#card-hashtags").val(hashtags);
+			}
+		}
+		else {
+			let index = hashtags.indexOf(val);
+			hashtags.splice(index, 1);
+			$("#card-hashtags").val(hashtags);
+		}
+	});
+
+	$("#card-externship").on('click', () => {
+		let val = "#externship";
+		if($("#card-externship").is(':checked')) {
+			if(!hashtags.includes(val)) {
+				hashtags.push(val);
+				$("#card-hashtags").val(hashtags);
+			}
+		}
+		else {
+			let index = hashtags.indexOf(val);
+			hashtags.splice(index, 1);
+			$("#card-hashtags").val(hashtags);
+		}
+	});
 	// $(".post-card").animate({ "opacity": "1"}, "slow", () => {
 
 	// });
@@ -36,12 +101,16 @@ let loggedIn = loggedInLinkedIn || loggedInFB;
 
 });
 
+
 $("#post-card-submit").on('click', () => {
 	console.log('post card submitted');
 $("#post-card-toggle").show();
 $(".post-card").animate({ "opacity": "0"}, "slow", () => {
-	cardOut = true;
 });
+	let type = 'class';
+	if($('#card-pro').is(':checked')) {
+		type = "company";
+	}
 	let json = {
 		author: {
 			firstName: $('#card-first-name').val(),
@@ -55,7 +124,7 @@ $(".post-card").animate({ "opacity": "0"}, "slow", () => {
 		picUrl: $('#card-picture-url').val(),
 		type: $('#card-type').val(),
 		description: $('#card-description').val(),
-		hashtags: $('#card-hashtags').val().split(" ")
+		hashtags: $('#card-hashtags').val().split(",")
 	};
 	console.log('New post = ',json)
 	window.feed.createNewPost(json)
@@ -71,7 +140,8 @@ $(".post-card").animate({ "opacity": "0"}, "slow", () => {
 	// });
 });
 
-$("#post-card-cancel").on('click', () => {
+$(".post-card-cancel").on('click', () => {
+	console.log('cancel');
 	$('.post-card').hide();
 	$("#post-card-toggle").show();
 });
